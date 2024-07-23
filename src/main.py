f339afdaab7e2d20ac20ebca46a9aac1ba2dc3a5
dianitaj285 <55167782+dianitaj285@users.noreply.github.com>
2024-07-22 20:53:06 -0500

    table user and role added. Seeds for role

--- src/main.py
+++ src/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
-from pony.orm import Database, Required, db_session
-from models.role import Role
+from pony.orm import Database, Required, Set, db_session
+from database_setup import db
 
 app = FastAPI()
 db = Database()
@@ -11,6 +11,17 @@ db.bind(provider='postgres', user='postgres', password='', host='localhost', dat
 @app.get("/")
 async def root():
     return {"message": "Hello World this is me using python for the first time"}
+
+
+class Role(db.Entity):
+    name=Required(str, unique=True)
+    users = Set('User')  
+
+class User(db.Entity):
+    name=Required(str, unique=True)
+    email=Required(str, unique=True)
+    role_id=Required(Role)
+
 
 db.generate_mapping(create_tables=True)
 
--- src/models/role.py
+++
@@ -1,7 +0,0 @@
-from database_setup import db
-from pony.orm import Required
-
-
-class Role(db.Entity):
-    name=Required(str, unique=True)
-
